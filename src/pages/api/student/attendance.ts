003c860118622de27a3a08a1509d91d347c7845f
Vaibhav Gawad <58821506+vaibhav31s@users.noreply.github.com>
2023-03-31 04:26:16 +0530

    working landing page and attendace api

--- src/pages/api/student/attendance.ts
+++ src/pages/api/student/attendance.ts
@@ -1,42 +1,47 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../utils/db";
 
+const getNextHour = (date: Date) => {
+  const nextHour = new Date(date);
+  nextHour.setHours(nextHour.getHours() + 1);
+  return nextHour;
+};
+
 
 const getattendance = async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-      const { name, email, avatar } = req.body;
+      const { sdate, edate , name, email, avatar } = req.body;
       const startDate = new Date('2000-01-01');
-      const endDate = new Date('2023-03-31');
-      const allStudents = await prisma.attendance.findMany({
+      const endDate = new Date();
+      const siddefault = '6425de68fd38788230c30f6b';
+    
+    //subject wise attendance
+    const allStudents = await prisma.attendance.count({
         where: {
-          AND: [
-            { date: { gte: startDate } },
-            { date: { lte: endDate } },
-            
-          ],
-          studentId :'6419a239df6b1521c178ff25'
-         
+            AND: [
+                { date: { gte: startDate } },
+                { date: { lte: endDate } },
+                {subject:'AIDS'}
+            ],
+            studentId :siddefault
         },
-        orderBy: {
-          date: 'asc',
-        },
-      });
+    });
 
-      let countDays = 0;
-        let countPresent = 0;
-        let aa = new Set<string>();
-        allStudents.forEach((student) => {
-            const date = student.date.toString();
-            const date1 = date.slice(4, 15).split(' ').join('-');
-            aa.add(date1);
-        });
-        const totalDays = aa.size * 3;
-        allStudents.forEach((student) => {
-            if (student.status) {
-                countPresent += 1;
-            }
-        });
-        const ans = (countPresent / totalDays) * 100;
+    const countsidattendace = await prisma.attendance.count({
+        where: {
+            AND: [  
+                { date: { gte: startDate } },
+                { date: { lte: endDate } },
+               
+            ], studentId: '6425852ba9e5259a2ed5089a'
+          }
+        } 
+        );
+        
+
+
+
+        const ans = (countsidattendace / allStudents) * 100;
         console.log(ans);
         
 
--- src/pages/api/teacher/manual.ts
+++ src/pages/api/teacher/manual.ts
@@ -1,54 +1,69 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../utils/db";
 
-
 const makeAttendance = async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        const { date, status, rollno,  subject} = req.body;
-        console.log(date, status, rollno,  subject);
+  try {
+    const { date, status, rollno, subject } = req.body;
+    console.log(date, status, rollno, subject);
 
-        const datas = rollno.split(',');
-        console.log(datas);
+    const datas = rollno.split(",");
+    console.log(datas);
 
-        while(datas.length > 0) {
-        const sid = await prisma.user.findUnique({
-            where: {
-                rollno: datas.pop()
+    while (datas.length > 0) {
+      const sid = await prisma.user.findUnique({
+        where: {
+          rollno: datas.pop(),
+        },
+        select: {
+          id: true,
+        },
+      });
+      console.log(sid);
+      // make attendance
+      //string to date
+      const date1 = new Date(date.substring(0, 10));
+      // status to boolean
+      const status1 = status === "present" ? true : false;
+
+      //check if allready present
+      const check = await prisma.attendance.findMany({
+        where: {
+          date: {
+            gte: date1,
+          },
+          studentId: sid?.id,
+          subject: subject,
+        },
+      });
+
+      if (check.length > 0) {
+        console.log("already present");
+        continue;
+      }
+
+      const date2 = new Date(date);
+
+      await prisma.attendance.create({
+        data: {
+          date: date2,
+          status: status1,
+          student: {
+            connect: {
+              id: sid?.id,
             },
-            select: {
-                id: true
-            }
-        });
-        console.log(sid);
-        // make attendance
-        //string to date
-        const date1 = new Date(date);
-        // status to boolean
-        const status1 = status === 'present' ? true : false;
-         await prisma.attendance.create({
-            data: {
-                date: date1,
-                status: status1,
-                student: {
-                    connect: {
-                        id: sid.id
-                    }
-                },
-                subject : subject
-            }
-        });
-        // console.log(attendance);
+          },
+          subject: subject,
+        },
+      });
+      // console.log(attendance);
     }
 
-
-       
-
-        res.status(200).json('done');
-    } catch (err) {
-      console.log(err);
-      res.status(500).json({ message: "Internal server error" });
-    }   
-  };
+    res.status(200).json("done");
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
 
 export default async function handler(
   req: NextApiRequest,
@@ -57,10 +72,9 @@ export default async function handler(
   const method = req.method;
   switch (method) {
     case "GET":
-       
       break;
     case "POST":
-        makeAttendance(req, res);
+      makeAttendance(req, res);
       break;
     case "PATCH":
       break;
